2cc0c6fed802d1ddbea260d857b90f1d46176631
Ben Trigg <orgrand@gmail.com>
2013-07-15 01:34:18 +0000

    web.js now pulls from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync('index.html');
+
+console.log(data);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
